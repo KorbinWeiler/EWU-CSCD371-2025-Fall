f23402a024e8622ddbafddf554ef373006e1c827
KorbinWeiler <korbinweiler@gmail.com>
2025-10-25 14:49:14 -0700

    Began implementing Node class

---
+++ GenericsHomework/Node.cs
@@ -0,0 +1,33 @@
+﻿namespace GenericsHomework
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next {
+            get;
+            
+            //Idk if this actually works or not
+            private set
+            {
+                if (value is not null)
+                {
+                    Next = value;
+                }
+                else
+                {
+                    Next = this;
+                }
+            } }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString() ?? "null";
+        }
+    }
+}
