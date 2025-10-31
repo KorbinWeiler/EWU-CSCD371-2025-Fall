6c94f61dfd3b4349343ecd70a20c518d15723e92
KorbinWeiler <korbinweiler@gmail.com>
2025-10-30 22:59:54 -0700

    More fixes

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -1,69 +1,67 @@
-﻿namespace GenericsHomework
+﻿namespace GenericsHomework;
+public class Node<T>
 {
-    public class Node<T>
+    public T Data { get; set; }
+    private Node<T> _next;
+    public Node<T> Next
     {
-        public T Data { get; set; }
-        private Node<T> _next;
-        public Node<T> Next
-        {
-            get => _next;
-            private set => _next = value ?? this;
-        }
+        get => _next;
+        private set => _next = value ?? this;
+    }
 
-        public Node(T data)
-        {
-            Data = data;
-            _next = this;
-        }
+    public Node(T data)
+    {
+        Data = data;
+        _next = this;
+    }
 
-        public void Append(T value)
+    public void Append(T value)
+    {
+        Node<T> nextNode = new(value)
         {
-            Node<T> nextNode = new(value)
+            Next = this.Next
+        };
+        Next = nextNode;
+    }
+
+    public void Clear()
+    {
+        Next = this;
+    }
+    public void ItterativeClear()
+    {
+        Node<T> cur = this;
+        Node<T>? prev = null;
+        while (cur.Next != this)
+        {
+            cur = cur.Next;
+            prev = cur;
+            prev.Next = prev;
+        }
+    }
+
+    public bool Exists(T value)
+    {
+        Node<T> cur = this;
+        do
+        {
+            if (Equals(cur.Data, value))
             {
-                Next = this.Next
-            };
-            Next = nextNode;
-        }
-
-        public void Clear()
-        {
-            Next = this;
-        }
-        public void ItterativeClear()
-        {
-            Node<T> cur = this;
-            Node<T>? prev = null;
-            while (cur.Next != this)
-            {
-                cur = cur.Next;
-                prev = cur;
-                prev.Next = prev;
+                return true;
             }
-        }
-
-        public bool Exists(T value)
+            cur = cur.Next;
+        } while (cur.Next != this);
+        return false;
+    }
+    public override string ToString()
+    {
+        if (Data is null)
         {
-            Node<T> cur = this;
-            do
-            {
-                if (Equals(cur.Data, value))
-                {
-                    return true;
-                }
-                cur = cur.Next;
-            } while (cur.Next != this);
-            return false;
+            return "null";
         }
-        public override string ToString()
+        else
         {
-            if (Data is null)
-            {
-                return "null";
-            }
-            else
-            {
-                return Data.ToString() ?? "null";
-            }
+            return Data.ToString() ?? "null";
         }
     }
 }
