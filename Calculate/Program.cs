fdd6aef58855b1380977e291e2616ccb8dff4f89
KorbinWeiler <korbinweiler@gmail.com>
2025-11-02 17:34:50 -0800

    Add Program Class

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
+
+namespace Calculate;
+
+public class Program
+{
+    private Func<string> ReadLine { get; set; }
+    private Func<bool> WriteLine { get; set; }
+
+    public Program()
+    {
+        ReadLine = () => { return Console.ReadLine(); };
+        WriteLine = () => { Console.WriteLine(); return true; };
+    }
+
+    public Program(Func<string> readLine, Func<bool> writeLine)
+    {
+        ReadLine = readLine;
+        WriteLine = writeLine;
+    }
+}
